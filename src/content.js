4b2596fd1851130e4f3f2445e06d8235bcb331e3
Nataliya Stepanova <gnykka@gmail.com>
2019-07-10 10:42:58 +0800

    More real behaviour of toaster

--- src/content.js
+++ src/content.js
@@ -1,70 +1,123 @@
-const pxToM = 0.000264;
-const width = window.innerWidth;
-const height = window.innerHeight;
-const spriteSize = 64;
-const toaster = chrome.extension.getURL('assets/toaster.png');
-
 let scroll = 0;
 let prevScrollTop = 0;
 
-const scrollInfo = document.createElement('div');
-scrollInfo.className = '__se-container';
-scrollInfo.innerText = '0 см';
-document.body.appendChild(scrollInfo);
+let phase = 0;
+const times = [10000, 10000, 10000, 10000]; // in ms
 
-const createSprite = () => {
-  const sprite = document.createElement('div');
+const stickerVisibleTime = 2000;
+const toasterAnimationTime = 500;
 
-  sprite.className = '__se-sprite';
-  sprite.style['background-image'] = `url('${toaster}')`;
-  sprite.style.left = `${width}px`;
-  sprite.style.transform = 'translateX(0)';
-  sprite.style.top = `${Math.random() * (height - spriteSize)}px`;
+// create container for stickers
+const sticker = document.createElement('div');
+sticker.className = '__se-sticker';
+sticker.innerText = '';
+document.body.appendChild(sticker);
 
-  document.body.appendChild(sprite);
-
-  let left = 0;
-  const speed = Math.random() * 10 + 1;
-
-  const flyStep = () => {
-    left -= speed;
-    sprite.style.transform = `translateX(${left}px)`;
-
-    if (left >= -width) {
-      requestAnimationFrame(flyStep);
-    } else {
-      document.body.removeChild(sprite);
-    }
-  };
-  requestAnimationFrame(flyStep);
-};
-
-setInterval(() => {
-  createSprite();
-}, 3000);
+// creater toaster
+const sprite = document.createElement('div');
+const toaster = chrome.extension.getURL('assets/toaster.png');
+sprite.className = '__se-sprite';
+sprite.style['background-image'] = `url('${toaster}')`;
+document.body.appendChild(sprite);
 
 const onScroll = debounce((event) => {
   const { scrollTop } = event.target.scrollingElement;
 
   scroll += Math.abs(scrollTop - prevScrollTop);
   prevScrollTop = scrollTop;
-
-  const m = scroll * pxToM;
-  const fullM = Math.floor(m);
-  const sm = Math.floor((m - fullM) * 100);
-
-  let str = '';
-  if (fullM > 0) {
-    str += `${fullM} м`;
-  }
-  if (fullM > 0 && sm > 0) {
-    str += ' ';
-  }
-  if (sm > 0 || str === '') {
-    str += `${sm} см`;
-  }
-
-  scrollInfo.innerHTML = str;
 }, this);
 
+const showSticker = (text) => {
+  sticker.innerText = text;
+  sticker.classList.add('visible');
+
+  setTimeout(() => { sticker.classList.remove('visible'); }, stickerVisibleTime);
+};
+
+const firstPhase = () => {
+  // nothing here except for simple animation
+  setTimeout(secondPhase, times[phase]);
+};
+
+const secondPhase = () => {
+  phase += 1;
+
+  // show text for start of second phase
+  showSticker('Закончилась первая фаза, начало второй фазы');
+
+  // toaster starts to jump a little
+  // calculate full jumps time and stop animation after it
+  const jumpsCount = Math.floor(times[phase] / toasterAnimationTime);
+  const jumpStep = (jump) => {
+    const top = (jump % 2 === 0 ? -20 : 0);
+    sprite.style.transform = `translateY(${top}px)`;
+
+    if (jump < jumpsCount * 2 - 1) {
+      setTimeout(() => { jumpStep(jump + 1); }, toasterAnimationTime / 2);
+    }
+  };
+  jumpStep(0);
+
+  // go to third phase after some time
+  setTimeout(thirdPhase, times[phase]);
+};
+
+const thirdPhase = () => {
+  phase += 1;
+
+  // show text for start of third phase
+  showSticker('Закончилась вторая фаза, начало третьей фазы');
+
+  // toaster jumps higher
+  const jumpsCount = Math.floor(times[phase] / toasterAnimationTime);
+  const jumpStep = (jump) => {
+    const top = (jump % 2 === 0 ? -60 : 0);
+    sprite.style.transform = `translateY(${top}px)`;
+
+    if (jump < jumpsCount * 2 - 1) {
+      setTimeout(() => { jumpStep(jump + 1); }, toasterAnimationTime / 2);
+    }
+  };
+  jumpStep(0);
+
+  // go to forth phase after some time
+  setTimeout(forthPhase, times[phase]);
+};
+
+const forthPhase = () => {
+  phase += 1;
+
+  // show text for start of forth phase
+  showSticker('Закончилась третья фаза, начало четвёртой фазы');
+
+  // toaster shifts
+  const shiftsCount = Math.floor(times[phase] / toasterAnimationTime);
+  const shiftStep = (shift) => {
+    const left = (shift % 2 === 0 ? 50 : 0);
+    sprite.style.transform = `translateX(${left}px)`;
+
+    if (shift < shiftsCount * 2 - 1) {
+      setTimeout(() => { shiftStep(shift + 1); }, toasterAnimationTime / 2);
+    }
+  };
+  shiftStep(0);
+
+  // go to fifth phase after some time
+  setTimeout(fifthPhase, times[phase]);
+};
+
+const fifthPhase = () => {
+  phase += 1;
+
+  // show text for start of forth phase
+  showSticker('Закончилась последняя, четвёртая фаза');
+
+  // toaster exits
+  sprite.classList.add('hidden');
+};
+
+// start calculating scroll amount
 window.addEventListener('scroll', onScroll, true);
+
+// start the phases
+firstPhase();
