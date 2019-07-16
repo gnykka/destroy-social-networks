fe8178f59abf2274595090b262692641a2dd1e7e
Nataliya Stepanova <gnykka@gmail.com>
2019-07-16 10:03:44 -0700

    Add serfer instead of toaster

--- src/content.js
+++ src/content.js
@@ -13,11 +13,11 @@ sticker.className = '__se-sticker';
 sticker.innerText = '';
 document.body.appendChild(sticker);
 
-// creater toaster
+// create serfer
 const sprite = document.createElement('div');
-const toaster = chrome.extension.getURL('assets/images/toaster.png');
+const serfer = chrome.extension.getURL('assets/images/serf-1.png');
 sprite.className = '__se-sprite';
-sprite.style['background-image'] = `url('${toaster}')`;
+sprite.style['background-image'] = `url('${serfer}')`;
 document.body.appendChild(sprite);
 
 const onScroll = debounce((event) => {
@@ -45,18 +45,7 @@ const secondPhase = () => {
   // show text for start of second phase
   showSticker('Закончилась первая фаза, начало второй фазы');
 
-  // toaster starts to jump a little
-  // calculate full jumps time and stop animation after it
-  const jumpsCount = Math.floor(times[phase] / toasterAnimationTime);
-  const jumpStep = (jump) => {
-    const top = (jump % 2 === 0 ? -20 : 0);
-    sprite.style.transform = `translateY(${top}px)`;
-
-    if (jump < jumpsCount * 2 - 1) {
-      setTimeout(() => { jumpStep(jump + 1); }, toasterAnimationTime / 2);
-    }
-  };
-  jumpStep(0);
+  // TODO: change serfer animation
 
   // go to third phase after some time
   setTimeout(thirdPhase, times[phase]);
@@ -68,17 +57,7 @@ const thirdPhase = () => {
   // show text for start of third phase
   showSticker('Закончилась вторая фаза, начало третьей фазы');
 
-  // toaster jumps higher
-  const jumpsCount = Math.floor(times[phase] / toasterAnimationTime);
-  const jumpStep = (jump) => {
-    const top = (jump % 2 === 0 ? -60 : 0);
-    sprite.style.transform = `translateY(${top}px)`;
-
-    if (jump < jumpsCount * 2 - 1) {
-      setTimeout(() => { jumpStep(jump + 1); }, toasterAnimationTime / 2);
-    }
-  };
-  jumpStep(0);
+  // TODO: change serfer animation
 
   // go to forth phase after some time
   setTimeout(forthPhase, times[phase]);
@@ -90,17 +69,7 @@ const forthPhase = () => {
   // show text for start of forth phase
   showSticker('Закончилась третья фаза, начало четвёртой фазы');
 
-  // toaster shifts
-  const shiftsCount = Math.floor(times[phase] / toasterAnimationTime);
-  const shiftStep = (shift) => {
-    const left = (shift % 2 === 0 ? 50 : 0);
-    sprite.style.transform = `translateX(${left}px)`;
-
-    if (shift < shiftsCount * 2 - 1) {
-      setTimeout(() => { shiftStep(shift + 1); }, toasterAnimationTime / 2);
-    }
-  };
-  shiftStep(0);
+  // TODO: change serfer animation
 
   // go to fifth phase after some time
   setTimeout(fifthPhase, times[phase]);
