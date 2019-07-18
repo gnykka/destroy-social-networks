a69ff88a023e4612b572043c08a6353e21b56e8e
Nataliya Stepanova <gnykka@gmail.com>
2019-07-18 10:37:29 -0700

    Save scroll amount to storage

--- src/content.js
+++ src/content.js
@@ -1,31 +1,66 @@
-let scroll = 0;
-let prevScrollTop = 0;
-
 let phase = 0;
 const times = [10000, 10000, 10000, 10000]; // in ms
 
+const storageUpdateTime = 5000;
 const stickerVisibleTime = 2000;
 const toasterAnimationTime = 500;
 
-// create container for stickers
-const sticker = document.createElement('div');
-sticker.className = '__se-sticker';
-sticker.innerText = '';
-document.body.appendChild(sticker);
+let sticker, sprite, serfer;
 
-// create serfer
-const sprite = document.createElement('div');
-const serfer = chrome.extension.getURL('assets/images/serf-1.png');
-sprite.className = '__se-sprite';
-sprite.style['background-image'] = `url('${serfer}')`;
-document.body.appendChild(sprite);
+// get scroll and time values
+let scroll, fullScroll;
+let time, fullTime;
+let prevScrollTop = 0;
+
+let today = new Date();
+let dateTime = (new Date(today.getFullYear(), today.getMonth(), today.getDate())).getTime();
+
+chrome.storage.sync.get(['scroll', 'full_scroll'], (items) => {
+  scroll = items.scroll || 0;
+  fullScroll = items.full_scroll || 0;
+});
+
+// write new values to storage
+const storageTimer = setInterval(() => {
+  today = new Date();
+  const nextDateTime = (new Date(today.getFullYear(), today.getMonth(), today.getDate())).getTime();
+
+  if (nextDateTime > dateTime) {
+    scroll = 0;
+    dateTime = nextDateTime;
+  }
+
+  chrome.storage.sync.set({ scroll: scroll, full_scroll: fullScroll });
+}, storageUpdateTime);
 
 const onScroll = debounce((event) => {
   const { scrollTop } = event.target.scrollingElement;
 
-  scroll += Math.abs(scrollTop - prevScrollTop);
+  delta = Math.abs(scrollTop - prevScrollTop);
+  scroll += delta;
+  fullScroll += delta;
   prevScrollTop = scrollTop;
 }, this);
+
+const onUnload = () => {
+  window.removeEventListener(onScroll);
+  clearInterval(storageTimer);
+}
+
+const renderResources = () => {
+  // create container for stickers
+  sticker = document.createElement('div');
+  sticker.className = '__se-sticker';
+  sticker.innerText = '';
+  document.body.appendChild(sticker);
+
+  // create serfer
+  sprite = document.createElement('div');
+  serfer = chrome.extension.getURL('assets/images/serf-1.png');
+  sprite.className = '__se-sprite';
+  sprite.style['background-image'] = `url('${serfer}')`;
+  document.body.appendChild(sprite);
+}
 
 const showSticker = (text) => {
   sticker.innerText = text;
@@ -35,58 +70,40 @@ const showSticker = (text) => {
 };
 
 const firstPhase = () => {
-  // nothing here except for simple animation
   setTimeout(secondPhase, times[phase]);
 };
 
 const secondPhase = () => {
   phase += 1;
-
-  // show text for start of second phase
   showSticker('Закончилась первая фаза, начало второй фазы');
-
-  // TODO: change serfer animation
-
-  // go to third phase after some time
   setTimeout(thirdPhase, times[phase]);
 };
 
 const thirdPhase = () => {
   phase += 1;
-
-  // show text for start of third phase
   showSticker('Закончилась вторая фаза, начало третьей фазы');
-
-  // TODO: change serfer animation
-
-  // go to forth phase after some time
   setTimeout(forthPhase, times[phase]);
 };
 
 const forthPhase = () => {
   phase += 1;
-
-  // show text for start of forth phase
   showSticker('Закончилась третья фаза, начало четвёртой фазы');
-
-  // TODO: change serfer animation
-
-  // go to fifth phase after some time
   setTimeout(fifthPhase, times[phase]);
 };
 
 const fifthPhase = () => {
   phase += 1;
-
-  // show text for start of forth phase
   showSticker('Закончилась последняя, четвёртая фаза');
-
-  // toaster exits
   sprite.classList.add('hidden');
 };
 
-// start calculating scroll amount
+// start calculating scroll
 window.addEventListener('scroll', onScroll, true);
+// remove interval before unload
+window.addEventListener('beforeunload', onUnload, true);
+
+// create stickers and serfer
+renderResources();
 
 // start the phases
 firstPhase();
